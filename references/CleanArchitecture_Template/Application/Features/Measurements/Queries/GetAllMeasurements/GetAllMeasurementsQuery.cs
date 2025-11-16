afe0d6dc37832ea61c2527027b3018609dc8469d
IxI-Enki <cell.enohp.apps@gmail.com>
2025-11-16 17:32:23 +0100

    feat: Initialize Clean Architecture template with core components
    - Added solution file and project structure for Clean Architecture.
    - Implemented initial API setup including controllers, models, and data access layers.
    - Introduced measurement and sensor entities with respective repositories and commands.
    - Included validation and exception handling mechanisms.
    - Added sample data files for measurements and initial migrations for database setup.
    - Established dependency injection and application services for better modularity.

---
+++ references/CleanArchitecture_Template/Application/Features/Measurements/Queries/GetAllMeasurements/GetAllMeasurementsQuery.cs
@@ -0,0 +1,8 @@
+using Application.Common.Results;
+using Application.Features.Dtos;
+using MediatR;
+
+namespace Application.Features.Measurements.Queries.GetAllMeasurements;
+
+public record GetAllMeasurementsQuery :
+    IRequest<Result<IReadOnlyCollection<GetMeasurementDto>>>;
